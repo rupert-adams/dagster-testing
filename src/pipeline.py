09828dd2951ca9aa9ccf78e33b0f1481c7c08950
Rupert Adams <rupert.adams@sainsburys.co.uk>
2019-09-26 13:51:21 +0200

    made a pipeline where one solid depends on another solid

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,12 +1,20 @@
 from dagster import execute_pipeline, pipeline, solid
 
+
 @solid
-def hello_world(_):
-    return 'hello'
+def solid_one(_):
+    return 'foo'
+
+
+@solid
+def solid_two(_, arg_one):
+    return arg_one * 2
+
 
 @pipeline
-def hello_world_pipeline():
-    hello_world()
+def hello_dagster_pipeline():
+    return solid_two(solid_one())
+
 
 if __name__ == '__main__':
     result = execute_pipeline(hello_world_pipeline)
