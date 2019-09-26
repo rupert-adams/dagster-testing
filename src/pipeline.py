6777d6f4c5d8f7fc1d7ec5efc99c07db21e6a5c2
Rupert Adams <rupert.adams@sainsburys.co.uk>
2019-09-26 14:00:00 +0200

    created an actual dag pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,20 +2,31 @@ from dagster import execute_pipeline, pipeline, solid
 
 
 @solid
-def solid_one(_):
-    return 'foo'
+def return_one(_):
+    return 1
 
 
 @solid
-def solid_two(_, arg_one):
-    return arg_one * 2
+def multiply_by_two(_, arg_a):
+    return arg_a * 2
+
+
+@solid
+def multiply_by_three(_, arg_a):
+    return arg_a * 3
+
+
+@solid
+def multiply(_, arg_b, arg_c):
+    return arg_b * arg_c
 
 
 @pipeline
-def hello_dagster_pipeline():
-    return solid_two(solid_one())
+def actual_dag_pipeline():
+    one = return_one()
+    multiply(multiply_by_two(one), multiply_by_three(one))
 
 
 if __name__ == '__main__':
-    result = execute_pipeline(hello_dagster_pipeline)
+    result = execute_pipeline(actual_dag_pipeline)
     assert result.success
