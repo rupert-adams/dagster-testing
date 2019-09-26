4498df686aebca8aa84b5dfddb60a80ae9a31e9c
Rupert Adams <rupert.adams@sainsburys.co.uk>
2019-09-26 17:15:20 +0200

    added context config to the hello world pipeline

--- src/hello_world_pipeline.py
+++ src/hello_world_pipeline.py
@@ -1,11 +1,16 @@
-from dagster import String, execute_pipeline, pipeline, solid
+from dagster import Field, execute_pipeline, pipeline, solid
 
 
-@solid
-def add_hello_to_world(_, word: String) -> String:
-    return 'Hello, ' + word + '!'
+@solid(config={'language': Field(str, is_optional=True, default_value='en-us')})
+def hello_with_config(context):
+    if context.solid_config['language'] == 'haw':
+        return 'Aloha honua!'
+    elif context.solid_config['language'] == 'cn':
+        return '你好, 世界!'
+    else:
+        return 'Hello, world!'
 
 
 @pipeline
-def hello_inputs_pipeline():
-    add_hello_to_world()
+def hello_with_config_pipeline():
+    hello_with_config()
